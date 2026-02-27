c4ae1a278d8b15c34943d5bae3c2cef2340cd546
kartikey <kartikeysingh99999@gmail.com>
2026-02-27 14:51:34 +0530

    dual firebase setup local + render

--- server/firebase.js
+++ server/firebase.js
@@ -1,14 +1,40 @@
+// const admin = require("firebase-admin");
+// const path = require("path");
+
+// // Firebase project (fuel-verification-efff2) ki key ka path
+// const serviceAccount = require("./firebase-key.json");
+
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount)
+//   });
+// }
+
+// const db = admin.firestore();
+// module.exports = db;
+
 const admin = require("firebase-admin");
 const path = require("path");
 
-// Firebase project (fuel-verification-efff2) ki key ka path
-const serviceAccount = require("./firebase-key.json");
+let serviceAccount;
+
+// ✅ If running on Render (ENV exists)
+if (process.env.FIREBASE_KEY) {
+  console.log("🔥 Using Firebase ENV credentials");
+  serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+} 
+// ✅ Otherwise use local file
+else {
+  console.log("💻 Using Local Firebase key file");
+  serviceAccount = require("./firebase-key.json");
+}
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
 const db = admin.firestore();
+
 module.exports = db;
